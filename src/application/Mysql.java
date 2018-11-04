2f689df9205229c346858b0b1f2777e294f5b7bf
henrique.fernandes <henrique.fernandes@NTB0046.conectcar.local>
2018-11-04 01:29:10 -0200

    teste

---
+++ src/application/Mysql.java
@@ -0,0 +1,171 @@
+package application;
+
+import java.sql.Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+  
+ 
+//Início da classe de conexão//
+ 
+public class Mysql {
+	Statement stmt = null;
+	 
+             public static String status = "Não conectou...";
+ 
+//Método Construtor da Classe//
+ 
+             Mysql() {
+ 
+    }
+ 
+  
+   public ResultSet select(String sql) throws SQLException {
+	   	   
+	      ResultSet rs = stmt.executeQuery(sql);
+	      //STEP 5: Extract data from result set
+	      /*while(rs.next()){
+	         //Retrieve by column name
+	         int id  = rs.getInt("id");
+	         int age = rs.getInt("age");
+	         String first = rs.getString("first");
+	         String last = rs.getString("last");
+
+	         //Display values
+	         System.out.print("ID: " + id);
+	         System.out.print(", Age: " + age);
+	         System.out.print(", First: " + first);
+	         System.out.println(", Last: " + last);
+	      }
+	      rs.close();*/
+	      
+	      
+	      return rs;
+	   
+   }
+             
+             
+ 
+//Método de Conexão//
+ 
+public static java.sql.Connection getConexaoMySQL() {
+ 
+        Connection connection = null;          //atributo do tipo Connection
+ 
+  
+ 
+try {
+ 
+// Carregando o JDBC Driver padrão
+ 
+String driverName = "com.mysql.jdbc.Driver";                        
+ 
+Class.forName(driverName);
+ 
+  
+ 
+// Configurando a nossa conexão com um banco de dados//
+ 
+            String serverName = "10.143.16.89";    //caminho do servidor do BD
+ 
+            String mydatabase ="adpass";        //nome do seu banco de dados
+ 
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+ 
+            String username = "root";        //nome de um usuário de seu BD      
+ 
+            String password = "55Mudaca";      //sua senha de acesso
+ 
+            connection = DriverManager.getConnection(url, username, password);
+ 
+  
+ 
+            //Testa sua conexão//  
+ 
+            if (connection != null) {
+ 
+                status = ("STATUS--->Conectado com sucesso!");
+ 
+            } else {
+ 
+                status = ("STATUS--->Não foi possivel realizar conexão");
+ 
+            }
+ 
+  
+ 
+            return connection;
+ 
+  
+ 
+        } catch (ClassNotFoundException e) {  //Driver não encontrado
+ 
+  
+ 
+            System.out.println("O driver expecificado nao foi encontrado.");
+ 
+            return null;
+ 
+        } catch (SQLException e) {
+ 
+//Não conseguindo se conectar ao banco
+ 
+            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+ 
+            return null;
+ 
+        }
+ 
+  
+ 
+    }
+ 
+  
+ 
+    //Método que retorna o status da sua conexão//
+ 
+    public static String statusConection() {
+ 
+        return status;
+ 
+    }
+ 
+   
+ 
+   //Método que fecha sua conexão//
+ 
+    public static boolean FecharConexao() {
+ 
+        try {
+ 
+            Mysql.getConexaoMySQL().close();
+ 
+            return true;
+ 
+        } catch (SQLException e) {
+ 
+            return false;
+ 
+        }
+ 
+  
+ 
+    }
+ 
+   
+ 
+   //Método que reinicia sua conexão//
+ 
+    public static java.sql.Connection ReiniciarConexao() {
+ 
+        FecharConexao();
+  
+        return Mysql.getConexaoMySQL();
+ 
+    }
+ 
+}
